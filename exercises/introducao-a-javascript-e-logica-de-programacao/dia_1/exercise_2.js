b98cd9e74f2456efd6657cd890f99765f41b169d
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-05-19 20:09:20 -0300

    Adicionei o enunciado do exercicio no topo de cada arquivo

--- exercises/introducao-a-javascript-e-logica-de-programacao/dia_1/exercise_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao/dia_1/exercise_2.js
@@ -1,3 +1,6 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+
 const a = 10;
 const b = 5;
 
--- exercises/introducao-a-javascript-e-logica-de-programacao/dia_1/exercise_8.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao/dia_1/exercise_8.js
@@ -1,3 +1,6 @@
+/* Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if */
+
 const a = 3;
 const b = 6;
 const c = 5;
