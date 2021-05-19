4f2dbf302c8670f1524740a000107087199218df
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-05-19 18:40:07 -0300

    Final exercicio 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao/dia_1/exercise_1.js
@@ -0,0 +1,22 @@
+const a = 2;
+const b = 3;
+
+// Adição
+const soma = a + b;
+console.log(soma);
+
+// Subtração
+const diferenca = a - b;
+console.log(diferenca);
+
+//Multiplicação
+const produto = a * b;
+console.log(produto);
+
+//Divisão
+const razao = a / b;
+console.log(razao);
+
+//Módulo
+const resto = a % b;
+console.log(resto);
