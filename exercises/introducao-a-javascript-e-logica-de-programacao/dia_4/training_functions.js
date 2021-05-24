c52fff4d22f19cdcc8b8516be9e8f4836ff79cd7
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-05-24 15:43:03 -0300

    Exercicio 1 do dia 1 transformado em funçao

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao/dia_4/training_functions.js
@@ -0,0 +1,23 @@
+// Transforme em funções os exercício do dia 1 desse bloco
+
+function somar(a, b) {
+  return a + b;
+}
+
+function subtrair(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
+
