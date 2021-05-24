902c47082dbab68ff5824e645d766781069969f2
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-05-24 18:13:28 -0300

    Finalizado topico 5 e final da parte I

--- exercises/introducao-a-javascript-e-logica-de-programacao/dia_4/parteI_Objects_ForIn.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao/dia_4/parteI_Objects_ForIn.js
@@ -26,3 +26,21 @@ for(let key in info) {
 for(let key in info) {
   console.log(info[key]);
 }
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for(let key in info) {
+  if(key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+  }
+  else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+}
