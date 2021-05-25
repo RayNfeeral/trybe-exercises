34a65004609e0433adca2e508150cd2b88165fa5
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-05-24 21:23:18 -0300

    Finalizado topico bonus 2

--- exercises/introducao-a-javascript-e-logica-de-programacao/dia_4/bonus.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao/dia_4/bonus.js
@@ -31,3 +31,24 @@ function romanParser(romanString) {
 }
 
 console.log(romanParser('MXLIII'));
+
+// Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro. Através de um loop for , percorra essa variável, busque os números pares e os adicione a um novo array que deverá ser retornado ao final pela pela função.
+
+
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+function arrayOfNumber(vector) {
+  const result = [];
+
+  for(let array of vector) {
+    for(let number of array) {
+      if(number % 2 === 0) {
+        result.push(number);
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(arrayOfNumber(vector));
