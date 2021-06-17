876021a848386b7ee2e834e06f8d572311c6e3d4
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-17 18:52:14 -0300

    Parte II - Exercicio 3 - Concluido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/part2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/part2.js
@@ -9,3 +9,16 @@ console.log(fatorial(5));
 const longestWord = (string) => string.split(' ').reduce((longest, current) => longest.length > current.length ? longest : current, '');
 
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+// 3. Crie uma página que contenha:
+// Um botão ao qual será associado um event listener ;
+// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+let clickCount = 0;
+
+window.onload = () => {
+  document.getElementById('count').addEventListener('click', (event) => {
+    clickCount += 1;
+    event.target.innerHTML = `Você me clicou: ${clickCount} vezes`;
+  });
+}
