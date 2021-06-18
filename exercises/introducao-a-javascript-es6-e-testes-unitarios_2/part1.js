06d341eee80def8e53459cbeb2e242288d178a5c
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-18 18:30:48 -0300

    Parte I - Exercicio 1 - Concluido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/part1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/part1.js
@@ -1,3 +1,10 @@
+// 1. Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+// Note que o parâmetro da função já está sendo passado na chamada da função.
+// 2. Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
+// Modifique o nome da pessoa compradora.
+// Modifique o valor total da compra para R$ 50,00.
+
+
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -34,9 +41,10 @@ const order = {
   },
 };
 
+
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
 }
 
 customerInfo(order);
