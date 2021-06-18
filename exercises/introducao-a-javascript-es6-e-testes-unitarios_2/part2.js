0351e0da4b6219c60ed4ff1742660d8b1afa85c1
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-18 19:22:55 -0300

    Parte II - Exercicio 1 - Concluido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/part2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/part2.js
@@ -17,3 +17,10 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+// 1. Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+function addProperty(object, key, value) {
+  object[key] = value;
+}
+
+addProperty(lesson2, 'turno', 'manhã');
