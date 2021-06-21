27e46ad8f386d4691e45e3df2c9feeb39b266a96
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-21 18:03:35 -0300

    Parte I - Exercicio 4 - Concluido

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.3/myFizzBuzz.test.js
@@ -0,0 +1,22 @@
+// 4. A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz', 'A função deveria retornar "fizzbuzz"');
+assert.strictEqual(myFizzBuzz(3), 'fizz', 'A função deveria retornar "fizz"');
+assert.strictEqual(myFizzBuzz(5), 'buzz', 'A função deveria retornar "buzz"');
+assert.strictEqual(myFizzBuzz(4), 4, 'A função deveria retornar 4');
+assert.strictEqual(myFizzBuzz('4'), false, 'A função deveria retornar false');
