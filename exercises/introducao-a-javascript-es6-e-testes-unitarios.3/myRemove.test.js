103f4e270077198106c21c2dc6a8198901203b24
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-21 17:53:13 -0300

    Parte I - Exercicio 2 - Concluido

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.3/myRemove.test.js
@@ -0,0 +1,24 @@
+// 2. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], 'Deveria retornar o array [1, 2, 4]');
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'Não deveria retornar o array [1, 2, 3, 4]');
+const arr = [1, 2, 3, 4];
+myRemove(arr, 3);
+assert.deepStrictEqual(arr, [1, 2, 3, 4], 'O array original não deve sofrer alterações');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4], 'Deveria retornar o array [1, 2, 3, 4]');
