891ec6fdde7c0dee2cc195caa5996f457ea76904
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-21 18:18:41 -0300

    Parte II - Exercicio 3 - Concluido

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.3/summAllNumbers.test.js
@@ -0,0 +1,11 @@
+// 3. Escreva a função sumAllNumbers para passar nos testes já implementados.
+const assert = require('assert');
+// escreva a função sumAllNumbers aqui
+const sumAllNumbers = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
+
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
