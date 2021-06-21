6fb9d406168ce8a81bda4043840aa0419e43d638
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-21 19:37:14 -0300

    Parte III - Exercicio 1 - Concluido

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios.3/greetPeople.test.js
@@ -0,0 +1,15 @@
+// 1. Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+const greetPeople = (people) => {
+  let greeting = 'Hello ';
+
+  const greetedPeople = people.map(person => greeting + person);
+
+  return greetedPeople;
+};
+
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+const assert = require('assert');
+
+assert.deepStrictEqual(greetPeople(parameter), result);
