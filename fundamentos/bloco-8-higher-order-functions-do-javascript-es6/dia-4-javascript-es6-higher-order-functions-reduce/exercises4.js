7d8e3cbab97eae61b02f9fa035f01ae399c37fe5
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-28 18:54:14 -0300

    exercicio 4 - concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercises4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercises4.js
@@ -2,8 +2,22 @@ const assert = require('assert');
 const books = require('./books');
 
 // 4 - Encontre o livro com o maior nome.
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
 function longestNamedBook() {
   // escreva seu código aqui
+  return books.reduce((acc, crr) => {
+    return acc.name.length < crr.name.length ? crr : acc;
+  });
 }
 
 assert.deepStrictEqual(longestNamedBook(), expectedResult);
