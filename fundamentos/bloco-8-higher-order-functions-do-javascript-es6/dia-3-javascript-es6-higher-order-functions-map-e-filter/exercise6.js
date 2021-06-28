59632364cda72a9fe22b22bcd2e45299d410a18b
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-28 19:36:52 -0300

    exercicio 6 - concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
@@ -10,6 +10,7 @@ const expectedResult = [
 
 function oldBooks() {
   // escreva seu código aqui
+  return books.filter(({ releaseYear }) => (2021 - releaseYear) > 60).map(({ name }) => name);
 }
 
 assert.deepStrictEqual(oldBooks(), expectedResult);
