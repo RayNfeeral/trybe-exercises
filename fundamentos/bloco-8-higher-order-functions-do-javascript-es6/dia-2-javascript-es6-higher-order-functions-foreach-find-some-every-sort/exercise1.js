e575ad450fbdd24fd60c3fa359717050952f2dfd
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-06-28 17:36:00 -0300

    Iniciando exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -0,0 +1,10 @@
+import { books } from './books';
+import assert from 'assert';
+
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+// Dica: use a função find.
+function authorBornIn1947() {
+  // escreva aqui o seu código
+}
+
+assert.strictEqual(authorBornIn1947(), 'Stephen King');
