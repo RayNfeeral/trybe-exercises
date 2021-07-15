cb243414d95ee5bfe3628d68bb2efd255018449e
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-15 19:05:47 -0300

    exercicio 2 - concluido

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/random.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/random.test.js
@@ -1,4 +1,5 @@
 // 1. Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
+// const { jest, expect } = require('@jest/globals');
 let random = require('./random');
 
 random = jest.fn().mockReturnValue(10);
@@ -9,5 +10,13 @@ describe('When called random', () => {
     expect(random).toHaveBeenCalled();
     expect(random()).toBe(10);
     expect(random).toHaveBeenCalledTimes(2);
+  });
+
+  // 2. Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
+  it('should return the result of the division of its first parameter by its second once', () => {
+    random.mockImplementationOnce((a, b) => a / b);
+
+    expect(random(10, 2)).toBe(5);
+    expect(random(10, 2)).toBe(10);
   });
 });
