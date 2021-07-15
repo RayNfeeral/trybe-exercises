50cbfee76d25aac02e29dc22ad3880da705dc07c
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-15 17:30:11 -0300

    iniciando exercicio 6

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/animals.js
@@ -0,0 +1,13 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => (
+  // Adicione o código aqui.
+);
+
+const getAnimal = (name) => {
+  // Adicione o código aqui.
+};
