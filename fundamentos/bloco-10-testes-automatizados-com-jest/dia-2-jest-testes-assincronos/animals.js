66b973346cae450788938611956b66136da52b2c
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-15 18:05:36 -0300

    exercicio 6 - concluido

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/animals.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/animals.js
@@ -5,9 +5,37 @@ const Animals = [
 ];
 
 const findAnimalByName = (name) => (
-  // Adicione o código aqui.
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animalFound = Animals.find((animal) => animal.name === name);
+      if (animalFound) {
+        return resolve(animalFound);
+      };
+
+      return reject('Nenhum animal com esse nome!');
+    }, 100);
+  })
 );
 
-const getAnimal = (name) => {
-  // Adicione o código aqui.
+const findAnimalsByAge = (age) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.filter((animal) => animal.age >= age);
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      };
+
+      return reject(`Nenhum animal com pelo menos ${age} anos de idade!`);
+    }, 100);
+  })
+);
+
+const getAnimal = async (name) => {
+  return findAnimalByName(name);
 };
+
+const getAnimals = async (age) => {
+  return findAnimalsByAge(age);
+};
+
+module.exports = { getAnimal, getAnimals };
--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/animals.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/animals.test.js
@@ -1,3 +1,5 @@
+const { getAnimal, getAnimals } = require('./animals');
+
 describe('Testando promise - findAnimalByName', () => {
   describe('Quando existe o animal com o nome procurado', () => {
     test('Retorne o objeto do animal', () => {
@@ -13,6 +15,26 @@ describe('Testando promise - findAnimalByName', () => {
       expect.assertions(1);
       return getAnimal('Bob').catch(error =>
         expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
+
+describe('Testando promise - findAnimalByAge', () => {
+  describe('Quando existem animais com a idade mínima pesquisada', () => {
+    test('Retorne um array com os objetos dos animais', () => {
+      expect.assertions(1);
+      return getAnimals(2).then(animals => {
+        expect(animals).toEqual([{ name: 'Soneca', age: 2, type: 'Dog' }, { name: 'Preguiça', age: 5, type: 'Cat' }]);
+      });
+    });
+  });
+
+  describe('Quando não existem animais com a idade mínima pesquisada', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimals(6).catch(error =>
+        expect(error).toEqual('Nenhum animal com pelo menos 6 anos de idade!')
       );
     });
   });
