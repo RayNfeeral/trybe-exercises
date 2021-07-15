cceb2063d82a61346d2122bca6c52493cbe6a7c4
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-15 19:16:54 -0300

    exercicio 3 - concluido

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/random.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/random.test.js
@@ -1,5 +1,5 @@
 // 1. Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
-// const { jest, expect } = require('@jest/globals');
+// const { expect, jest } = require('@jest/globals');
 let random = require('./random');
 
 random = jest.fn().mockReturnValue(10);
@@ -19,4 +19,21 @@ describe('When called random', () => {
     expect(random(10, 2)).toBe(5);
     expect(random(10, 2)).toBe(10);
   });
+
+  // 3. Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+  it('should return the multiplication of its 3 parameters', () => {
+    random.mockImplementation((a, b, c) => a * b *c);
+    
+    expect(random(4, 5, 6)).toBe(120);
+  });
+  
+  it('should duplicate its parameter', () => {
+    random.mockReset();
+    
+    expect(random()).toBe(undefined);
+    
+    random.mockImplementation((a) => 2 * a);
+
+    expect(random(3)).toBe(6);
+  })
 });
