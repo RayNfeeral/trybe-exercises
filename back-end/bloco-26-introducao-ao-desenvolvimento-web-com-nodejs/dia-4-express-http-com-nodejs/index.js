7a707d5e12ed5d5c48277d6369c3453c351708cd
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-12-03 19:21:29 -0300

    exercicio 2 - concluido

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 // 1. Crie uma rota GET /ping
 // Sua rota deve retornar o seguinte JSON: { message: 'pong' }
-app.get('/ping', (req, res, _next) => res.status(201).json({ message: 'pong' }));
+app.get('/ping', (_req, res, _next) => res.status(201).json({ message: 'pong' }));
+
+// 2. Crie uma rota POST /hello
+// Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+// Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
+app.post('/hello', (req, res, _next) => {
+  const { name } = req.body;
+
+  return res.status(201).json({ message: `Hello, ${name}!`});
+});
 
 app.listen(3000, () => console.log('Escutando'));
